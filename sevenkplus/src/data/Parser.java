fe5e69949c19eb46e688108ce3c7376985134dc2
Scott Wu <scott.wu46@gmail.com>
2015-06-12 00:38:56 -0700

    Parser navigates through files in /logs/

--- sevenkplus/src/data/Parser.java
+++ sevenkplus/src/data/Parser.java
@@ -1,7 +1,23 @@
 package data;
 
-public class Parser {
-  public Parser(String log) {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Parser {
+  private static File folder = new File("../logs");
+
+  public static void main(String[] args) throws IOException {
+    System.out.println(folder.getName());
+
+    File[] listOfFiles = folder.listFiles();
+    for (File file : listOfFiles) {
+      if (file.isFile()) {
+        System.out.println("Parsing " + file.getName());
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        System.out.println(in.readLine());
+      }
+    }
   }
 }
