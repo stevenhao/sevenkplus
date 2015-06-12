8c547baff13b64498e1781c4d2defe48331abea0
Scott Wu <scott.wu46@gmail.com>
2015-06-11 23:38:49 -0700

    Add create-db script

---
+++ scripts/create_db.sh
@@ -0,0 +1,11 @@
+read -p "Username: " uname
+read -sp "Password: " pass
+
+mysql --user="$uname" --password="$pass" -Bse "
+drop database if exists sevenkplus;
+create database sevenkplus;
+use sevenkplus;
+create table player (id int primary key auto_increment, name varchar(255));
+insert into player (name) values ('sevenk');
+insert into player (name) values ('stevenk');"
+echo 'Success!'
