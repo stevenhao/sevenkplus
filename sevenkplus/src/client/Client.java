d513dceb0d6cdaeffcf3ddbac97dcd3361af3291
Scott Wu <scott.wu46@gmail.com>
2015-06-11 22:29:25 -0700

    Fix formatting

--- sevenkplus/src/client/Client.java
+++ sevenkplus/src/client/Client.java
@@ -7,19 +7,18 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-	public static void main(String[] args) {
-		int portNumber = args.length >= 1 ? Integer.parseInt(args[0]) : 5000;
-		String hostName = "localhost";
-		try {			
-			System.out.println("connecting to port " + portNumber);
-		    Socket socket = new Socket(hostName, portNumber);
-		    System.out.println("connected.");
-		    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-		    BufferedReader in = new BufferedReader(
-		        new InputStreamReader(socket.getInputStream()));
-		    System.out.println(in.readLine());
-		} catch (IOException e) {
-			System.err.println(e);
-		}
-	}
+  public static void main(String[] args) {
+    int portNumber = args.length >= 1 ? Integer.parseInt(args[0]) : 5000;
+    String hostName = "localhost";
+    try {
+      System.out.println("connecting to port " + portNumber);
+      Socket socket = new Socket(hostName, portNumber);
+      System.out.println("connected.");
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      System.out.println(in.readLine());
+    } catch (IOException e) {
+      System.err.println(e);
+    }
+  }
 }
--- sevenkplus/src/server/Server.java
+++ sevenkplus/src/server/Server.java
@@ -1,7 +1,5 @@
 package server;
 
-import static org.jooq.generated.Tables.PLAYER;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -17,63 +15,61 @@ import org.jooq.Result;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
+import static org.jooq.generated.Tables.PLAYER;
+
 public class Server {
-	private int port;
-	private Socket clientSocket;
-	private ServerSocket serverSocket;
-	private PrintWriter out;
-	private BufferedReader in;
-	
-	public Server(int port) {
-		this.port = port;
-	}
-	
-	private void connectToDB() {
-        String userName = "root";
-        String password = "";
-        String url = "jdbc:mysql://localhost:3306/sevenkplus";
+  private final int port;
+  private Socket clientSocket;
+  private ServerSocket serverSocket;
+  private PrintWriter out;
+  private BufferedReader in;
 
-        // Connection is the only JDBC resource that we need
-        // PreparedStatement and ResultSet are handled by jOOQ, internally
-        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
-        	DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
-        	Result<Record> result = create.select().from(PLAYER).fetch();
-        	for (Record r : result) {
-        	    Integer id = r.getValue(PLAYER.ID);
-        	    String name = r.getValue(PLAYER.NAME);
-        	    System.out.println("ID: " + id + " title: " + name);
-        	}
-        }
+  public Server(int port) {
+    this.port = port;
+  }
 
-        // For the sake of this tutorial, let's keep exception handling simple
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-	}
-	
-	private void connectToClient() {
-		try {			
-			serverSocket = new ServerSocket(port);
-			clientSocket = serverSocket.accept();
-			out = new PrintWriter(
-					clientSocket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(
-					clientSocket.getInputStream()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		out.println("Sup Client.");
-		out.flush();
-	}
-	
-	public static void main(String[] args) {
-		int portNumber = args.length >= 1 ? Integer.parseInt(args[0]) : 5000;
-		System.out.println("Serving at port " + portNumber);
-		Server server = new Server(portNumber);
-		System.out.println("Server started.");		
-		server.connectToDB();
-		System.out.println("Connected to database.");
-		server.connectToClient();
-		System.out.println("Connected to client.");
-	}
+  private void connectToDB() {
+    // TODO: move these to config file
+    String userName = "root";
+    String password = "";
+    String url = "jdbc:mysql://localhost:3306/sevenkplus";
+
+    // Connection is the only JDBC resource that we need
+    // PreparedStatement and ResultSet are handled by jOOQ, internally
+    try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+      DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+      Result<Record> result = create.select().from(PLAYER).fetch();
+      for (Record r : result) {
+        Integer id = r.getValue(PLAYER.ID);
+        String name = r.getValue(PLAYER.NAME);
+        System.out.println("ID: " + id + " title: " + name);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void connectToClient() {
+    try {
+      serverSocket = new ServerSocket(port);
+      clientSocket = serverSocket.accept();
+      out = new PrintWriter(clientSocket.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    out.println("Sup Client.");
+    out.flush();
+  }
+
+  public static void main(String[] args) {
+    int portNumber = args.length >= 1 ? Integer.parseInt(args[0]) : 5000;
+    System.out.println("Serving at port " + portNumber);
+    Server server = new Server(portNumber);
+    System.out.println("Server started.");
+    server.connectToDB();
+    System.out.println("Connected to database.");
+    server.connectToClient();
+    System.out.println("Connected to client.");
+  }
 }
