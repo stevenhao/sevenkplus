dd0a4c37e81b41075511cc04c16c0ecfd3fb9902
stevenhao <stevenhao@gmail.com>
2015-06-27 15:36:18 -0700

    Added prefs.conf, used for connecting to mysql
    TODO: make the create_db script also use it

--- sevenkplus/src/common/DatabaseController.java
+++ sevenkplus/src/common/DatabaseController.java
@@ -18,21 +18,20 @@ import org.jooq.generated.tables.Player;
 import org.jooq.impl.DSL;
 
 public class DatabaseController {
-  // TODO: move these to config file
-  private final static String userName = "root";
-  private final static String password = "";
-  private final static String url = "jdbc:mysql://localhost:3306/sevenkplus";
+
   private final DSLContext db;
-  private final static Logger logger = Logger.getLogger(DatabaseController.class.getName());
+  private final static Logger logger = Logger
+      .getLogger(DatabaseController.class.getName());
 
   public DatabaseController() throws SQLException {
     db = getDatabaseContext();
   }
 
   private DSLContext getDatabaseContext() throws SQLException {
-    // Connection is the only JDBC resource that we need
-    // PreparedStatement and ResultSet are handled by jOOQ, internally
-    Connection conn = DriverManager.getConnection(url, userName, password);
+    String username = Utils.getMysqlUsername();
+    String password = Utils.getMysqlPassword();
+    String url = Utils.getMysqlUrl();
+    Connection conn = DriverManager.getConnection(url, username, password);
 
     logger.info("Connected to database");
     return DSL.using(conn, SQLDialect.MYSQL);
@@ -51,24 +50,28 @@ public class DatabaseController {
   }
 
   public Integer getPlayer(String playerName) {
-    Result<Record1<Integer>> playerIds = db.select(Player.PLAYER.ID).from(Player.PLAYER).where(
-        Player.PLAYER.NAME.equal(playerName)).fetch();
+    Result<Record1<Integer>> playerIds = db.select(Player.PLAYER.ID)
+        .from(Player.PLAYER).where(Player.PLAYER.NAME.equal(playerName))
+        .fetch();
 
     if (playerIds.isEmpty()) {
       // No record in the DB for this player yet
       return null;
     } else if (playerIds.size() > 1) {
-      throw new IllegalArgumentException("More than one ID found for player name!");
+      throw new IllegalArgumentException(
+          "More than one ID found for player name!");
     }
 
     return playerIds.get(0).value1();
   }
 
   public Integer getOrInsertPlayer(String playerName) {
-    // TODO: Represent player/hand data with local java objects and only update the DB at the end of
+    // TODO: Represent player/hand data with local java objects and only update
+    // the DB at the end of
     // each file. Cleaner and significantly improves performance
     if (getPlayer(playerName) == null) {
-      db.insertInto(Player.PLAYER, Player.PLAYER.NAME).values(playerName).execute();
+      db.insertInto(Player.PLAYER, Player.PLAYER.NAME).values(playerName)
+          .execute();
       logger.info("Added new player: " + playerName);
     }
 
@@ -76,12 +79,13 @@ public class DatabaseController {
   }
 
   public Integer getOrInsertHand(String handTag) {
-    Result<Record1<Integer>> handIds =
-        db.select(Hand.HAND.ID).from(Hand.HAND).where(Hand.HAND.TAG.equal(handTag)).fetch();
+    Result<Record1<Integer>> handIds = db.select(Hand.HAND.ID).from(Hand.HAND)
+        .where(Hand.HAND.TAG.equal(handTag)).fetch();
     if (handIds.isEmpty()) {
       db.insertInto(Hand.HAND, Hand.HAND.TAG).values(handTag).execute();
       logger.info("Added new hand: " + handTag);
-      handIds = db.select(Hand.HAND.ID).from(Hand.HAND).where(Hand.HAND.TAG.equal(handTag)).fetch();
+      handIds = db.select(Hand.HAND.ID).from(Hand.HAND)
+          .where(Hand.HAND.TAG.equal(handTag)).fetch();
     }
 
     if (handIds.size() > 1) {
@@ -97,35 +101,43 @@ public class DatabaseController {
       return;
     }
 
-    db.insertInto(Play.PLAY, Play.PLAY.PLAYERID, Play.PLAY.HANDID).values(playerId, handId)
-        .execute();
+    db.insertInto(Play.PLAY, Play.PLAY.PLAYERID, Play.PLAY.HANDID)
+        .values(playerId, handId).execute();
   }
 
   public void updateVPIP(int playerId, int handId) {
-    db.update(Play.PLAY).set(Play.PLAY.VPIP, (byte) 1).where(Play.PLAY.PLAYERID.equal(playerId)
-        .and(Play.PLAY.HANDID.equal(handId))).execute();
+    db.update(Play.PLAY)
+        .set(Play.PLAY.VPIP, (byte) 1)
+        .where(
+            Play.PLAY.PLAYERID.equal(playerId).and(
+                Play.PLAY.HANDID.equal(handId))).execute();
   }
 
   public void updatePFR(int playerId, int handId) {
-    db.update(Play.PLAY).set(Play.PLAY.PFR, (byte) 1).where(Play.PLAY.PLAYERID.equal(playerId)
-        .and(Play.PLAY.HANDID.equal(handId))).execute();
+    db.update(Play.PLAY)
+        .set(Play.PLAY.PFR, (byte) 1)
+        .where(
+            Play.PLAY.PLAYERID.equal(playerId).and(
+                Play.PLAY.HANDID.equal(handId))).execute();
   }
 
   public int getHands(int playerId) {
-    Record1<Integer> record =
-        db.selectCount().from(Play.PLAY).where(Play.PLAY.PLAYERID.equal(playerId)).fetchOne();
+    Record1<Integer> record = db.selectCount().from(Play.PLAY)
+        .where(Play.PLAY.PLAYERID.equal(playerId)).fetchOne();
     return (int) record.getValue(0);
   }
 
   public int getHandsVPIP(int playerId) {
     Record1<Integer> record = db.selectCount().from(Play.PLAY)
-        .where(Play.PLAY.PLAYERID.equal(playerId)).and(Play.PLAY.VPIP.equal((byte) 1)).fetchOne();
+        .where(Play.PLAY.PLAYERID.equal(playerId))
+        .and(Play.PLAY.VPIP.equal((byte) 1)).fetchOne();
     return (int) record.getValue(0);
   }
 
   public int getHandsPFR(int playerId) {
     Record1<Integer> record = db.selectCount().from(Play.PLAY)
-        .where(Play.PLAY.PLAYERID.equal(playerId)).and(Play.PLAY.PFR.equal((byte) 1)).fetchOne();
+        .where(Play.PLAY.PLAYERID.equal(playerId))
+        .and(Play.PLAY.PFR.equal((byte) 1)).fetchOne();
     return (int) record.getValue(0);
   }
 }
--- sevenkplus/src/common/Utils.java
+++ sevenkplus/src/common/Utils.java
@@ -1,8 +1,64 @@
 package common;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Utils {
+
+  static final Map<String, String> secrets;
+  static {
+    secrets = new HashMap<>();
+    try (BufferedReader in = new BufferedReader(new InputStreamReader(
+        new FileInputStream("prefs.conf")))) {
+      String line = null;
+      while ((line = in.readLine()) != null) {
+        int commentStart = line.indexOf("#");
+        if (commentStart != -1) {
+          line = line.substring(0, commentStart);
+        }
+        if (line.isEmpty()) {
+          continue;
+        }
+
+        String[] tokens = line.split("=");
+        if (tokens.length == 2) {
+          secrets.put(tokens[0].trim(), tokens[1].trim());
+        } else if (tokens.length == 0) {
+          // continue
+        } else {
+          System.err.println(line);
+          throw new Exception("error parsing config file");
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static String getSecret(String key) {
+    if (secrets.containsKey(key)) {
+      return secrets.get(key);
+    } else {
+      return "";
+    }
+  }
+
+  public static String getMysqlUsername() {
+    return secrets.get("mysql_username");
+  }
+
+  public static String getMysqlPassword() {
+    return secrets.get("mysql_password");
+  }
+
+  public static String getMysqlUrl() {
+    return secrets.get("mysql_url");
+  }
+
   public static String join(Collection<String> strings) {
     String result = "";
     for (String player : strings) {
@@ -20,6 +76,8 @@ public class Utils {
     }
 
     double ratio = numerator / ((double) denominator);
-    return numerator + " / " + denominator + " (" + String.format("%.1f", ratio * 100d) + "%)";
+    return numerator + " / " + denominator + " ("
+        + String.format("%.1f", ratio * 100d) + "%)";
   }
+
 }
